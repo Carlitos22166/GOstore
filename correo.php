b3e298e4f42342d8266dabdc01a848ba87e1fe55
Carlitos22166 <71353298+Carlitos22166@users.noreply.github.com>
2020-09-25 21:12:54 -0500

    Update correo.php

--- correo.php
+++ correo.php
@@ -6,7 +6,7 @@ $apellidos = $_POST['apellidos'];
 
 
 $header = $nombre . " " . $apellidos . "Gracias por tu preferencia";
-$mensajeCompleto = "\n\nAtentamente: " . $destinatario . "\n" ;
+$mensajeCompleto = "\n\nAtentamente: " . $email . "\n" ;
 
 mail($destinatario, $asunto, $mensajeCompleto, $header);
 echo "<script> setTimeout(\"location.href='index.html'\", 1000)</script>";
